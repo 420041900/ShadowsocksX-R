1ebd36e79b0e5eaf7f178d6a0f1f4e5532fb5423
Yicheng <yicheng.fzu@gmail.com>
2016-08-26 17:46:08 +0800

    stop ss-local when quie

--- ShadowsocksX-NG/AppDelegate.swift
+++ ShadowsocksX-NG/AppDelegate.swift
@@ -158,6 +158,7 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSUserNotificationCenterDele
 
     func applicationWillTerminate(aNotification: NSNotification) {
         // Insert code here to tear down your application
+        StopSSLocal()
     }
     
     func applyConfig() {
